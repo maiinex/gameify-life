dffe46953fe3a7a0298c9c3f19de9b14bbe9544f
Mainotas Perliuba <60159639+maiinex@users.noreply.github.com>
2026-05-06 22:51:29 +0200

    DB connection established
    installed supabase and created and tested connection to db

---
+++ backend/app/database.py
@@ -0,0 +1,10 @@
+from supabase import create_client, Client
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+url: str = os.environ.get("SUPABASE_URL")
+key: str = os.environ.get("SUPABASE_KEY")
+
+supabase: Client = create_client(url, key)
